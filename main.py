6bee11560d6407d383332add1af6c1b25dffea61
Prototype19 <daron.lebaredian@gmail.com>
2025-09-06 14:00:00 -0500

    First draft

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ def simple_work_calc(n, a, b):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+
+	# Base case:
+	if n <= 1:
+		return n
+	else:
+		return a*simple_work_calc(n//b, a, b) + n
 
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -32,8 +36,10 @@ def work_calc(n, a, b, f):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+	if n <= 1:
+		return 1
+	else:
+		return a * work_calc(n // b, a, b, f) + f(n)
 
 def span_calc(n, a, b, f):
 	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
@@ -47,8 +53,11 @@ def span_calc(n, a, b, f):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+	if n <= 1:
+		return 1
+	else:
+		return span_calc(n // b, a, b, f) + f(n)
+
 
 
 
--- test_main.py
+++ test_main.py
@@ -2,26 +2,41 @@ from main import *
 
 def test_simple_work():
 	""" done. """
-	assert simple_work_calc(10, 2, 2) == #TODO
-	assert simple_work_calc(20, 3, 2) == #TODO
-	assert simple_work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == 36
+	assert simple_work_calc(20, 3, 2) == 230
+	assert simple_work_calc(30, 4, 2) == 650
+
+	assert simple_work_calc(81, 2, 3) == 211
+	assert simple_work_calc(50, 10, 3) == 1710
+	assert simple_work_calc(10, 3,2) == 70
+
+
 
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert work_calc(10, 2, 2, lambda x: x) == 36
+	assert work_calc(10, 2, 2, lambda x: 1) == 15
+	assert work_calc(16, 2, 2, lambda x: x * x) == 496
+
+	assert work_calc(8, 3, 2, lambda x: x) == 65
+	assert work_calc(27, 3, 3, lambda x: 1) == 40
+	assert work_calc(16, 4, 2, lambda x: x * x) == 1280
 
 
 def test_compare_work():
 	# curry work_calc to create multiple work
 	# functions taht can be passed to compare_work
 
-	# create work_fn1
-	# create work_fn2
+	work_fn1 = lambda n: work_calc(n, 2, 2, lambda x: 1)  # f(n) = 1
+	work_fn2 = lambda n: work_calc(n, 2, 2, lambda x: x)  # f(n) = n
 	res = compare_work(work_fn1, work_fn2)
 
 	print(res)
 
 	
 def test_compare_span():
-	# TODO
+	work_fn1 = lambda n: span_calc(n, 2, 2, lambda x: 1)  # f(n) = 1
+	work_fn2 = lambda n: span_calc(n, 2, 2, lambda x: x)  # f(n) = n
+	res = compare_span(work_fn1, work_fn2)
+
+	print(res)
+
